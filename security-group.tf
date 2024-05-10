0530d4728984b542834ebcaff7980adc8c804589
MuneerBSK <muneerbsk96@gmail.com>
2024-05-10 10:50:49 -0400

    AutoPush

--- security-group.tf
+++ security-group.tf
@@ -6,16 +6,16 @@ resource "aws_security_group" "allow_redis" {
 
   ingress {
     description      = "Allows docdb from local network"
-    from_port        = 
-    to_port          = 
+    from_port        = 6379
+    to_port          = 6379
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
   ingress {
     description      = "Allows docdb from default network"
-    from_port        = 
-    to_port          = 
+    from_port        = 6379
+    to_port          = 6379
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
