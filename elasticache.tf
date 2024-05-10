85c24ac488d31c0f5e60ba3f21a8cc1963f2be9a
MuneerBSK <muneerbsk96@gmail.com>
2024-05-10 11:01:57 -0400

    AutoPush

--- elasticache.tf
+++ elasticache.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.2"
   port                 = 6379
   # security_group_ids   = [aws_security_group.allow_redis.id]
